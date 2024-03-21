0ff845cabc61642cd76ea396d01c0f364b6ed0a3
BigGafLeo <karoldzwon@gmail.com>
2024-03-21 15:35:55 +0100

    Test

---
+++ Test/App.js
@@ -0,0 +1,25 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "A",
+    };
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      text: this.state.text + "A",
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick.bind(this)}>Dodaj "A"</button>
+        <h1>{this.state.text}</h1>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
