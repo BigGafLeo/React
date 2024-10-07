89e772cff36d1d0960ff2f21c2a985962b26b261
BigGafLeo <karoldzwon@gmail.com>
2024-10-07 16:00:34 +0200

    Finished Api verison of place picker

--- Api_place_picker/src/App.jsx
+++ Api_place_picker/src/App.jsx
@@ -1,15 +1,22 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from "react";
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import Error from "./components/Error.jsx";
+
+import { updatedUserPlaces, fetchUserPlaces } from "./http.js";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setisFetching] = useState(false);
+  const [error, seterror] = useState();
+
+  const [errorUpdatingPlaces, seterrorUpdatingPlaces] = useState(null);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -22,7 +29,22 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  useEffect(() => {
+    async function fetchPlaces() {
+      console.log("tratatatata");
+      setisFetching(true);
+      try {
+        const fetchedUserPlaces = fetchUserPlaces();
+        setUserPlaces(fetchedUserPlaces);
+      } catch (error) {
+        console.log(error.message);
+        seterror({ message: error.message || "Failed to fetch user places" });
+      }
+      setisFetching(false);
+    }
+  }, []);
+
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,18 +54,57 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try {
+      await updatedUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      seterrorUpdatingPlaces({
+        message: error.message || "Failed to update places",
+      });
+      setUserPlaces(userPlaces);
+    }
   }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-    );
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id
+        )
+      );
 
-    setModalIsOpen(false);
-  }, []);
+      try {
+        await updatedUserPlaces(
+          userPlaces.filter((place) => place.id !== selectedPlace.id)
+        );
+      } catch (error) {
+        seterrorUpdatingPlaces({
+          message: error.message || "Failed to delete places",
+        });
+        setUserPlaces(userPlaces);
+      }
+
+      setModalIsOpen(false);
+    },
+    [userPlaces]
+  );
+
+  function handleError() {
+    seterrorUpdatingPlaces(null);
+  }
 
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && (
+          <Error
+            title="An error ocured"
+            message={errorUpdatingPlaces.message}
+            onConfirm={handleError}
+          />
+        )}
+      </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -60,12 +121,17 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+        {error && <Error title="Error ocured" message={error.message} />}
+        {!error && (
+          <Places
+            title="I'd like to visit ..."
+            fallbackText="Select the places you would like to visit below."
+            isLoading={isFetching}
+            loadingText="Fetichng your places..."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- Api_place_picker/src/components/AvailablePlaces.jsx
+++ Api_place_picker/src/components/AvailablePlaces.jsx
@@ -1,11 +1,53 @@
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+
+import { useState, useEffect } from "react";
+
+import { fetchAvaiblePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setisFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, seterror] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setisFetching(true);
+
+      try {
+        const places = await fetchAvaiblePlaces();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setisFetching(false);
+        });
+      } catch (error) {
+        seterror({ message: error.message || "dont fetch places" });
+        setisFetching(false);
+      }
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return (
+      <Error message={error.message} title={"Error ocured during fetichng"} />
+    );
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
+      loadingText="Fetching place data..."
+      isLoading={isFetching}
       onSelectPlace={onSelectPlace}
     />
   );
